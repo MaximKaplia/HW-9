e0643cf32f695a4f124c098c769356474b12d58d
Maxim Kaplia <kapliamaks@gmail.com>
2023-04-01 23:14:51 +0300

    initial commit

---
+++ HW9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW9
+//
+//  Created by Avariceral on 4/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
